821860ef8232ccdc4ea3060bb9e5f7f80d624eec
Vl-Truh <78600828+Vl-Truh@users.noreply.github.com>
2021-02-28 18:27:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ module.exports = function towelSort (matrix) {
   for (let i = 0; i < matrix.length; i++) {
       
     if (i % 2) { 
-    	for (let j = matrix.length ; j >= 0; j--) {
+    	for (let j = matrix.length - 1 ; j >= 0; j--) {
     	nMatrix.push(matrix[i][j]);
       }
     }
